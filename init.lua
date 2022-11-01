1cfd8a786a3f3824d9214718433e70fff4ea08c5
Rafael de Conde <GitHubRafael@gmail.com>
2022-11-01 01:42:13 -0300

    added shorcut for the inlay_hints of the rust-tool

--- init.lua
+++ init.lua
@@ -10,48 +10,40 @@
 
 local config = {
 
-  -- set vim options here (vim.<first_key>.<second_key> =  value)
-  plugins = {
-    init = {
-      { "lervag/vimtex" },
-      {
-        "simrat39/rust-tools.nvim",
-        config = function()
-          local rt = require("rust-tools")
-          rt.setup({
-            rt.inlay_hints.enable(),
-            server = {
-              on_attach = function(_, bufnr)
-                -- Hover actions
-                vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
-                -- Code action groups
-              end,
+    -- set vim options here (vim.<first_key>.<second_key> =  value)
+    plugins = {
+        init = {
+            { "lervag/vimtex" },
+            {
+                "simrat39/rust-tools.nvim",
             },
-          })
-        end
-      },
-    },
-    ["neo-tree"] = {
-      filesystem = {
-        filtered_items = {
-          visible = true
-        }
-      }
-    }
-  },
-  lsp = {
-    clangd = {
-      server = {
-        capabiblities = {
-          offsetEncoding = "utf-8"
         },
-      },
+        ["neo-tree"] = {
+            filesystem = {
+                filtered_items = {
+                    visible = true
+                }
+            }
+        },
     },
-  },
-  options = function(local_vim)
-    local_vim.opt.relativenumber = false
-    return local_vim
-  end,
+    mappings = {
+        n = {
+            ["<F12>"] = function() require("rust-tools").inlay_hints.enable() end,
+        },
+    },
+    lsp = {
+        clangd = {
+            server = {
+                capabiblities = {
+                    offsetEncoding = "utf-8"
+                },
+            },
+        },
+    },
+    options = function(local_vim)
+        local_vim.opt.relativenumber = true
+        return local_vim
+    end,
 }
 
 return config
