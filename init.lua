5073f44296bd9be28375854458bf691cf56bac6f
Rafael de Conde <GitHubRafael@gmail.com>
2023-01-02 01:09:52 -0300

    Config in the beginning of the year

--- init.lua
+++ init.lua
@@ -1,19 +1,9 @@
---              AstroNvim Configuration Table
--- All configuration changes should go inside of the table below
-
--- You can think of a Lua "table" as a dictionary like data structure the
--- normal format is "key = value". These also handle array like data structures
--- where a value with no key simply has an implicit numeric key
-
-
-
-
 local config = {
 
         -- set vim options here (vim.<first_key>.<second_key> =  value)
         plugins = {
                 init = {
-                        "catppuccin/nvim",
+                        -- "catppuccin/nvim",
                         { "lervag/vimtex" },
                 },
                 ["neo-tree"] = {
@@ -24,10 +14,10 @@ local config = {
                         }
                 },
         },
-        colorscheme = "catppuccin",
+        -- colorscheme = "catppuccin",
         mappings = {
                 n = {
-                        ["<F11>"] = function() vim.api.nvim_command("@a") end,
+                        ["<F12>"] = function() vim.api.nvim_command("@a") end,
                 },
         },
         lsp = {
@@ -40,8 +30,10 @@ local config = {
                 },
         },
         luasnip = {
-                vscode_snippet_paths = {
-                        "./lua/user/snippets",
+                vscode = {
+                        paths = {
+                                "./lua/user/snippets",
+                        },
                 },
         },
         options = function(local_vim)
